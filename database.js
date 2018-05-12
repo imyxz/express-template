7555335dc0abe675ac77de9e39d8547a7dcf7caa
imyxz <i@yxz.me>
2018-05-12 10:54:45 +0800

    Allow association configuration

--- database.js
+++ database.js
@@ -6,7 +6,8 @@ const hotSwapping = require('./hotSwapping')
 let sequelize;
 let definations;
 let models;
-let Logger = ()=>{};
+let Logger = () => {};
+
 function ConnectDB({
   db_name,
   username,
@@ -19,20 +20,36 @@ function ConnectDB({
     logging: Logger
   })
 }
-async function LoadDefination(dir, _sequelize) {
+
+async function LoadAssociation(dir, _definations) {
   let ret = {}
   let list = await fs.readdir(dir)
-  let promises = list.filter(e => {
+  list.filter(e => {
     return path.extname(e) === '.js'
-  }).map(e => {
-    let name = path.basename(e, '.js')
-    ret[name] = _sequelize.import(path.resolve(dir, e))
-    return ret[name].sync().then(xxx => {
-      console.info(`Load defination ${name} done.`)
-    })
+  }).forEach(e => {
+    let func = require(path.resolve(dir, e));
+    func(_definations)
+    console.info(`Load association in ${path.basename(e)} done.`)
   })
-  await Promise.all(promises)
-  return ret;
+}
+async function LoadDefination(dir, _sequelize) {
+  let _definations = {}
+  let list = await fs.readdir(dir)
+  list.filter(e => {
+    return path.extname(e) === '.js'
+  }).forEach(e => {
+    let name = path.basename(e, '.js')
+    console.info(`Loading defination ${name}...`)
+    _definations[name] = _sequelize.import(path.resolve(dir, e))
+  })
+  await LoadAssociation(path.resolve(dir, 'Association'), _definations)
+  let promises = Object.values(_definations).map(e => {
+    return e.sync()
+  })
+  await Promise.all(promises).then(xxx => {
+    console.info(`Load definations done.`)
+  })
+  return _definations;
 }
 async function LoadModel(dir, _definations) {
   let ret = {}
@@ -68,13 +85,12 @@ function AddHotSwappingForModels(dir, _models, _definations) {
     depth: 0
   })
   watcher.on('all', (event, filepath) => {
-    if (path.extname(filepath) === '.js')
-    {
+    if (path.extname(filepath) === '.js') {
       console.info(`Updating Model ${filepath}...`)
-      try{
+      try {
         dynamicReloadModel(filepath, _models, _definations)
         console.info('Done!')
-      }catch(e){
+      } catch (e) {
         console.error(`Error while updating ${filepath}`)
         console.error(e)
       }
@@ -85,12 +101,12 @@ module.exports = async function (Config) {
   let DBConfig = Config.database
   if (models !== undefined)
     return models;
-  if(Config.dev.showDBLog === true)
+  if (Config.dev.showDBLog === true)
     Logger = (e) => console.log(e)
   sequelize = ConnectDB(DBConfig)
   definations = await LoadDefination(path.resolve(__dirname, 'Model', 'Defination'), sequelize)
   models = await LoadModel(path.resolve(__dirname, 'Model'), definations)
-  if(Config.dev.hotSwap === true)
+  if (Config.dev.hotSwap === true)
     AddHotSwappingForModels(path.resolve(__dirname, 'Model'), models, definations)
   return models;
 }
