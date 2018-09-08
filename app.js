9eb326d3de2f51219709f71d877d8240c307020c
imyxz <i@yxz.me>
2018-09-08 17:18:43 +0800

    change app entry

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
-const Database = require('./database');
-let Config
-const Router = require('./router')
-const express = require('express');
-const fs = require('fs-extra');
-const path = require('path');
-const ConfigLoader = require('./configLoader');
-const hotSwapping = require('./hotSwapping');
-const ExpressAsyncWrapper = require('./ExpressAsyncWrapper');
-(async function (){
-  Config = ConfigLoader()
-  //expose to global to convenient for require module
-  global._require = hotSwapping._require
-  global.watchRequire = hotSwapping.watchRequire
-  const RouterBaseDir = path.resolve(__dirname, 'Route')
-  console.info("Loading Models....")
-  const DB = await Database(Config);
-  console.info("Done!\n")
-  const context = {
-    Model: DB.models,
-    Definition: DB.definitions,
-    Sequelize: DB.sequelize,
-    Config,
-    Wrapper: ExpressAsyncWrapper
-  }
-  console.info("Loading Routes....")
-  let [router, routerTree] = await Router(RouterBaseDir, context)
-  console.info(routerTree)
-  console.info("Done!\n")
-  let app = express()
-  app.use('/', router)
+const server = require('./server')
+const Config = require('./configLoader')();
+(async () => {
+  let {app} = await server()
   app.listen(Config.server.port, () => {
     console.info(`Server started at http://localhost:${Config.server.port}${Config.server.prefix || '/'}`)
   })
---
+++ server.js
@@ -0,0 +1,35 @@
+const hotSwapping = require('./hotSwapping');
+global._require = hotSwapping._require
+global.watchRequire = hotSwapping.watchRequire
+const Database = require('./database');
+let Config
+const Router = require('./router')
+const express = require('express');
+const fs = require('fs-extra');
+const path = require('path');
+const ConfigLoader = require('./configLoader');
+const ExpressAsyncWrapper = require('./ExpressAsyncWrapper');
+module.exports = async function (loadroutes = true) {
+  Config = ConfigLoader()
+  //expose to global to convenient for require module
+  const RouterBaseDir = path.resolve(__dirname, Config.server.route_dir)
+  console.info("Loading Models....")
+  const DB = await Database(Config);
+  console.info("Done!\n")
+  const context = {
+    Model: DB.models,
+    Definition: DB.definitions,
+    Sequelize: DB.sequelize,
+    Config,
+    Wrapper: ExpressAsyncWrapper
+  }
+  let app
+  if(loadroutes === true){
+    console.info("Loading Routes....")
+    let [router, routerTree] = await Router(RouterBaseDir, context)
+    console.info("Done!\n")
+    app = express()
+    app.use('/', router)
+  }
+  return {app, context}
+}
