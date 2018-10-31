02d897d73f476b19cc23f3c9fbc6a9c19113fc9a
imyxz <i@yxz.me>
2018-10-31 16:13:57 +0800

    update config loader

--- config-loader.js
+++ config-loader.js
@@ -1,10 +1,25 @@
+const mods = {
+  'development': './configs/config.development.js'
+}
+const env = {
+
+}
 module.exports = () => {
   let mod = process.argv[2]
   let Config
-  if (mod === 'development') {
-    Config = require('./configs/config.development.js')
-  } else if (mod === 'production') {
-    Config = require('./configs/config.production.js')
+  if (mod === 'env') {
+    const target = process.env.NODE_ENV
+    if (env.hasOwnProperty(target)) {
+      Config = require(env[target])
+    } else {
+      throw new Error('Not have target env:' + target)
+    }
+  } else {
+    if (mods.hasOwnProperty(mod)) {
+      Config = require(mods[mod])
+    } else {
+      throw new Error('Not have target mod:' + mod)
+    }
   }
   return Config
 }
